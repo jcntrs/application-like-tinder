9c3acd481a1c57247a3299c953dba30a66e12944
Jean C. Contreras <j.contreras.open@gmail.com>
2020-03-10 18:01:35 -0300

    Improve view of the main component

--- src/components/main.js
+++ src/components/main.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { Card } from 'antd';
 import { Button } from 'antd';
-import { HeartTwoTone } from '@ant-design/icons';
+import { HeartTwoTone, StarTwoTone, CloseOutlined, ReloadOutlined, FireTwoTone, UserOutlined, MessageTwoTone } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 const Main = () => {
     return (
         <div className="form-usuario">
-            <Card
+            <Card title={
+                <>
+                    <UserOutlined style={{ color: 'gray' }} />
+                    <FireTwoTone style={{ marginLeft: 70, fontSize: 21 }} twoToneColor="#eb2f96" />
+                    <MessageTwoTone style={{ marginLeft: 67 }} twoToneColor="#eb2f96" />
+                </>
+            }
                 hoverable
                 style={{ width: 240 }}
                 cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
             >
                 <Meta title="Europe Street beat" description="www.instagram.com" />
+                <Button style={{ marginTop: 15, marginLeft: 8, marginRight: 7 }} danger shape="circle" icon={<ReloadOutlined />} />
+                <Button style={{ marginTop: 15, marginLeft: 8, marginRight: 8 }} danger shape="circle" icon={<CloseOutlined />} />
+                <Button style={{ marginTop: 15, marginLeft: 8, marginRight: 8 }} danger shape="circle" icon={<HeartTwoTone twoToneColor="#eb2f96" />} />
+                <Button style={{ marginTop: 15, marginLeft: 7, marginRight: 8 }} danger shape="circle" icon={<StarTwoTone twoToneColor="#eb2f96" />} />
             </Card>
-            <Button type="primary" shape="circle">
-                A
-            </Button>
-            <Button danger shape="circle" icon={<HeartTwoTone twoToneColor="#eb2f96" />} />
         </div>
     );
 }
